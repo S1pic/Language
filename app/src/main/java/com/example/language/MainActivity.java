4687883b77d8eb7c68e49cbceec27d19a0717b97
Skye4 <makayangella@gmail.com>
2025-10-27 04:21:41 +0200

    used java css html, ensured the learn page,daily challenge,greetings and introductions work also loaded the 3 languages made sure the user can scroll down and go back

--- app/src/main/java/com/example/language/MainActivity.java
+++ app/src/main/java/com/example/language/MainActivity.java
@@ -2,5 +2,9 @@ package com.example.language;
 
 import com.getcapacitor.BridgeActivity;
 
+// This is the main activity for our Android application.
+// In a Capacitor project, the MainActivity extends BridgeActivity.
+// The BridgeActivity is special because it loads our web application (HTML, CSS, JavaScript)
+// and provides a bridge for web code to interact with native device features.
 public class MainActivity extends BridgeActivity {}
 
